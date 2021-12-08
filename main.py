838c66a09fc61f0a56b835c99225c5796bb1d8d9
Tom Rehnström <tomreh@ug.kth.se>
2021-12-08 13:10:13 +0100

    Initial commit.

---
+++ main.py
@@ -0,0 +1,6 @@
+import pygame
+from scenes.game import Game
+
+if __name__ == "__main__":
+    game = Game(8, 8)
+    print(game)
---
+++ scenes/game.py
@@ -0,0 +1,15 @@
+import pygame
+
+class Game:
+    def __init__(self, size_x, size_y) -> None:
+        self.width = size_x
+        self.height = size_y
+        self.array = [[0] * size_x] * size_y
+
+    def __repr__(self) -> str:
+        out_string = ""
+
+        for row in self.array:
+            out_string += (str(row) + '\n')
+
+        return out_string

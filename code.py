ab0ac7ccf33d07d6fcf020285b0be1922f011bb9
lefevre <lefevre@kth.se>
2021-12-09 18:56:06 +0100

    Added initial gameplay code to temporary .py-file

---
+++ code.py
@@ -0,0 +1,23 @@
+import keyboard
+
+
+class Agent:
+    def __init__(self):
+        pass
+    def Output(self, state):
+        pass
+
+class TestBot(Agent):
+    def Output(self, state):
+        pass
+
+class Player(Agent):
+    def __init__(self, keySet):
+        pass
+    def Output(self, state):
+        pass
+
+
+FORWARD = (0, 0)
+RIGHT = (0, 0)
+LEFT = (0, 0)

9ca061f999db0f30e81513f434f0a632423fafe8
Tom Rehnström <tomreh@ug.kth.se>
2021-12-09 23:27:44 +0100

    Add more

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ if __name__ == "__main__":
     pygame.init()
 
     game = Game(8, 8)
-    renderer = Renderer(game)
+    renderer = Renderer(game, 256, 256)
 
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-                
+
         renderer.draw()
--- modules/renderer.py
+++ modules/renderer.py
@@ -3,12 +3,41 @@ import pygame
 from modules.game import Game
 
 class Renderer:
-    def __init__(self, game: Game) -> None:
+    def __init__(self, game: Game, size_x: int, size_y: int) -> None:
         self.game = game
 
-        self.screen = pygame.display.set_mode((255, 255))
+        self.screen = pygame.display.set_mode((size_x, size_y))
+        self.screen_width = size_x
+        self.screen_height = size_y
+
+        self.rects = self.pre_gen_rects()
         pygame.display.set_caption('Snakes on INDA')
 
     def draw(self) -> None:
         self.screen.fill((255, 0, 255))
+
+        color: list = [0, 255, 255]
+
+        for row_index, row in enumerate(self.game.array):
+            for val_index, val in enumerate(row):
+                if val != 0:
+                    pygame.draw.rect(self.screen, color, self.rects[val_index][row_index])
+
         pygame.display.flip()
+
+    def pre_gen_rects(self) -> list[list]:
+        x_const: int = self.screen_width // self.game.width
+        y_const: int = self.screen_height // self.game.height
+
+        rect_list = []
+
+        for x_val in range(0, self.screen_width, x_const):
+            sub_list = []
+            for y_val in range(0, self.screen_height, y_const):
+                sub_list.append(pygame.rect.Rect(x_val, y_val, x_const, y_const))
+            
+            rect_list.append(sub_list)
+
+        return rect_list
+
+

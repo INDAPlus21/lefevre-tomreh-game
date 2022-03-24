8bc5a65d37eec5f8f8f4fe2936bf01549218b028
Tom Rehnström <tomreh@ug.kth.se>
2022-03-24 16:34:16 +0100

    Add working grid drawing system with cooler readme.

--- main.py
+++ main.py
@@ -5,11 +5,23 @@ if __name__ == '__main__':
     pygame.init()
     renderer = Renderer(400, 400)
 
+    test_grid = [[0] * 10 for i in range(10)]
+    test_grid[0][0] = 1
+    test_grid[9][9] = 1
+    test_grid[5][3] = 1
+    test_grid[3][5] = 1
+    test_grid[4][4] = 1
+    test_grid[5][5] = 1
+    test_grid[5][4] = 1
+    test_grid[4][5] = 1
+
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-                pygame.quit()
+                break;
 
-        renderer.draw()
+        renderer.draw(test_grid)
+
+pygame.quit()
--- modules/renderer.py
+++ modules/renderer.py
@@ -1,26 +1,41 @@
-from pygame import display
-from pygame import Surface
+from pygame import display, Rect, draw
 import json
 
 class Renderer:
     def __init__(self, w_width, w_height):
         # Not really needed
-        self.load_artistic()
+        self.d = self.load_artistic()
+
+        self.w_width = w_width
+        self.w_height = w_height
+        
+        self.rects = self.gen_rects(10)
 
         # Create window
-        self.window = display.set_mode((w_width + self.art_d["d"]["bt"] * 2
-                                        ,w_height + self.art_d["d"]["bt"] * 2))
+        self.window = display.set_mode((w_width + self.d["d"]["bt"] * 2
+                                        ,w_height + self.d["d"]["bt"] * 2))
+    
         display.set_caption("PvSnake")
 
-        self.draw_zone = self.window.subsurface((self.art_d["d"]["bt"], self.art_d["d"]["bt"]
+        self.draw_zone = self.window.subsurface((self.d["d"]["bt"], self.d["d"]["bt"]
                                                 ,w_width, w_height))
 
-        self.window.fill(self.art_d["c"]["br"])
+        self.window.fill(self.d["c"]["br"])
 
-
-    def draw(self):
+    def draw(self, grid):
         # Background color
-        self.draw_zone.fill(self.art_d["c"]["bg"])
+        self.draw_zone.fill(self.d["c"]["bg"])
+
+        x, y = 0, 0
+
+        for row in grid:
+            x = 0
+            for cell in row:
+                if cell == 1:
+                    draw.rect(self.draw_zone, self.d["c"]["cc"], self.rects[y][x])
+                x += 1
+            y += 1
+
 
         # Swap buffers, done last
         display.flip()
@@ -28,6 +43,17 @@ class Renderer:
     # Overcomplication kekw
     def load_artistic(self):
         with open("assets/artistic.json") as f:
-            self.art_d = json.load(f)
+            return json.load(f)
 
         
+    def gen_rects(self, size):
+        cell_size = self.w_width // size
+
+        rows = []
+        for val_y in range(0, self.w_width, cell_size):
+            row = []
+            for val_x in range(0, self.w_width, cell_size):
+                row.append(Rect(val_x, val_y, cell_size, cell_size))
+            rows.append(row)
+
+        return rows

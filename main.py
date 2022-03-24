2978c2ebba8951a86ad4d638a0ae98817bbc4283
Tom Rehnström <tomreh@ug.kth.se>
2022-03-24 15:16:45 +0100

    Basic window-managment feutures added to renderer, with some styling.

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello world")
+from modules.renderer import Renderer
+import pygame
+
+if __name__ == '__main__':
+    pygame.init()
+    renderer = Renderer(400, 400)
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                pygame.quit()
+
+        renderer.draw()
